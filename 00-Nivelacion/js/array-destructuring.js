30e102113f8cf1fd5084e9288702e06f47917b11
bluefox182 <fergaldoss@gmail.com>
2021-05-20 08:53:16 -0500

    spread

---
+++ 00-Nivelacion/js/array-destructuring.js
@@ -0,0 +1,15 @@
+// const users = ["Pepe", "Juan", "Luis", "Fer "];
+
+// document.write(users);
+
+// DESTRUCTURING
+
+const [user1, user2, user3, user4] = ["Pepe", "Juan", "Luis", "Fer "];
+
+document.write(user1);
+
+// Tambien podemos liberar memoria asi
+
+const [, , worker3] = ["Pepe", "Juan", "Luis", "Fer "];
+
+document.write(worker3);
---
+++ 00-Nivelacion/js/spread.js
@@ -0,0 +1,6 @@
+const fruits = ["manzana", "uva", "pera"];
+const citrus = ["naranja", "mandarina", "limon"];
+
+const new1 = [...fruits, ...citrus];
+
+document.write(new1);
