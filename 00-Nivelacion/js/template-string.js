083c63fc2b171bfe444df0d8c111fe4308b18a4b
bluefox182 <fergaldoss@gmail.com>
2021-05-20 09:05:09 -0500

    using backticks

---
+++ 00-Nivelacion/js/template-string.js
@@ -0,0 +1,7 @@
+const name = "Fernando";
+const year = 1984;
+const age = new Date().getFullYear() - year;
+
+console.log(`Welcome back ${name}, tu edad actual es de ${age}`);
+console.log(`Welcome back ${name}, tu edad actual es de ${age}`.toUpperCase());
+document.write(`Welcome back ${name}, tu edad actual es de ${age}`);
