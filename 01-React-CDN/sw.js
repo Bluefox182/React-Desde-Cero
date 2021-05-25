8d94979960e2fc085a3f43988ad3d3de9ada50f8
bluefox182 <fergaldoss@gmail.com>
2021-05-25 14:42:46 -0500

    instalando el SW

---
+++ 01-React-CDN/sw.js
@@ -0,0 +1,37 @@
+const CACHE_ELEMENTS = [
+  ",.",
+  "https://unpkg.com/react@17/umd/react.development.js",
+  "https://unpkg.com/react-dom@17/umd/react-dom.development.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./styles/style.css",
+  "./components/Contador.js",
+];
+
+const CACHE_NAME = "v1_contador_react";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch(console.log);
+    })
+  );
+});
+
+// EVENTO #2
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch(console.log);
+    })
+  );
+});
